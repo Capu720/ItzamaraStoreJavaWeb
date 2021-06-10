243bfc8ad49d385acf744fce906d73723515b2e9
Frunulunuuuu <69992683+Frunulunuuuu@users.noreply.github.com>
2021-06-10 01:47:05 -0500

    cambiando js

--- web/assets/js/catalogos.js
+++ web/assets/js/catalogos.js
@@ -3,6 +3,8 @@ window.addEventListener("load", load, false);
 function load() {
     document.getElementById("op1").addEventListener("click", function () { mostrar(1); });
     document.getElementById("op2").addEventListener("click", function () { mostrar(2); });
+    document.getElementById("btnCrear").addEventListener("click", function () { crear(); });
+    document.getElementById("btnRegresar").addEventListener("click", function () { regresar(); });
     //Se les asigana a todos los botones con Name="verProductos", la función mostrar(2);
     let botones = [];
     botones = document.getElementsByName("verProductos");
@@ -19,19 +21,35 @@ function mostrar(i) {
         case 1:
             document.getElementById("misCatalogos").style.display = "block";
             document.getElementById("Crear").style.display = "none";
+            document.getElementById("verProductos").style.display = "none";
             document.getElementById("op1").style.color =  "#e7abad";
             document.getElementById("op2").style.color =  "#2d2b2b";
             break;
         case 2:
             document.getElementById("misCatalogos").style.display = "none";
             document.getElementById("Crear").style.display = "block";
+            document.getElementById("verProductos").style.display = "none";
             document.getElementById("op1").style.color =  "#2d2b2b";
-            document.getElementById("op2").style.color =  "#e7abad";s
+            document.getElementById("op2").style.color =  "#e7abad";
             break;
     }
 }
 
 function verProductos() {
-    alert("Funcion en proceso");
+    document.getElementById("misCatalogos").style.display = "none";
+    document.getElementById("verProductos").style.display = "block";
 }
 
+function regresar(){
+    mostrar(1);
+}
+
+function crear() {
+    form = document.getElementById("idCrear");
+    if (!form.checkValidity()) {
+        alert("Llenar todos los campos correctamente");
+    }
+    else{
+        alert("Catálogo agregado");
+    }
+}
